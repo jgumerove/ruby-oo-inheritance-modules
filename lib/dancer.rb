0c7fa1d2b6eec671a01b386ebadfe415b1ce9106
jgumerove <josh.gumerove@gmail.com>
2021-03-03 13:20:20 -0500

    commit tree f339dd1cc6e33505cf623012245cd09bdf3b24e2

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,17 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+require_relative './fancy_dance.rb'
+
+class Dancer
+    extend FancyDance::ClassMethods
+    include FancyDance::InstanceMethods
+
+   #include Dance  -- previous less organized syntax
+   #extend MetaDancing  -- previous less organized syntax 
+  
+    attr_accessor :name
+  
+    def initialize(name)
+      @name = name
+    end
+  end 
